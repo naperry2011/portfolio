dbd9b31b53cc943c0304e6c848338db85ca93e7d
Nicholas Perry <nuperry2011@gmail.com>
2024-07-22 19:33:05 -0700

    updating portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPage = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll('nav ul li a');
+    
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
